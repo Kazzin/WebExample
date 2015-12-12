1bf564f8767290a3d0865aa6e2cc5fd05ad11f2c
alanwtf <alan.mathiasen@outlook.com>
2015-12-11 16:50:48 -0800

    Agregado de head.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-
+<?
+$title = "titulo del documento"; //creo una variable titulo
+include("include/head.php"); //incluyo lo que esta en include/head.php
+//algo que esta piola es poder hjacer esto
+//no tenes que especificar el tipo
+?>
 <p> Test </p>
 
 <body>
